0a588ad8e138655c20fb5a1b37e9c8a443c8ccfe
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-01 23:11:22 -0500

    corrected superhero mispelling preventing addition of data to database

--- superheroesdb/superheroesapp/views.py
+++ superheroesdb/superheroesapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
 
 # Create your views here.
 
 
 def index(request):
+    all_superheroesapp = Superhero.objects.all()
     return render(request, 'superheroesapp/index.html')
